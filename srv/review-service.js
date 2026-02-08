1fd9bf9ceef0bbbcb07acf701423d6ca3dced47d
MarianKijewski <mariankijewski@outlook.com>
2026-02-08 19:04:09 +0100

    Set order status to received when link is opened

--- srv/review-service.js
+++ srv/review-service.js
@@ -1,15 +1,16 @@
 const cds = require('@sap/cds')
 const { ref, columns, expand } = cds.ql
+const { Status } = require('../@cds-models/sap/cap/orders')
 
 module.exports = cds.service.impl(async function () {
   const { Orders } = this.entities;
-
   this.on('GET', Orders, async (req) => {
     const [ orderId ] = req.params;
     if (!orderId) {
       req.error(405);
     }
 
+    await UPDATE `Orders` .set `status = ${Status.Received}` .where(orderId)
     // Fetch order with items expanded
     const order = await SELECT.from `Orders` .where(orderId)
       .columns(
