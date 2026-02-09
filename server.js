33fcd68051ee3f9e79a7c5cecb4824275c368c99
Konstantinos Makariou <cmakario@student.42heilbronn.de>
2026-02-09 13:27:52 +0000

    set redirection to provider portal index

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const cds = require('@sap/cds')
 
 cds.on('bootstrap', (app) => {
   app.get('/', (req, res) => {
-    res.redirect('https://port4004-workspaces-ws-h7mxz.eu30.applicationstudio.cloud.sap/')
+    res.redirect('/com.schwarz.providerportal/index.html')
+    // Redirects the user to the specified path in the application
     // res.redirect('/com.schwarz.providerportal/test/flpSandbox.html#comschwarzproviderportal-tile')
   })
 })
